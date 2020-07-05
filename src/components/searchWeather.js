1142005d01d17bd81b46a103aa5421966f527447
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2020-07-05 17:21:39 -0400

    css

--- src/components/searchWeather.js
+++ src/components/searchWeather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../styles/searchWeather.css";
 
 const Weather = () => {
   let [responseObj, setResponseObj] = useState({});
@@ -16,9 +17,20 @@ const Weather = () => {
   }
   return (
     <div>
-      <h2>Find Current Weather Conditions</h2>
-      <div>{JSON.stringify(responseObj)}</div>
+      <h2>Atlanta Weather</h2>
       <button onClick={searchWeather}>Get Forecast</button>
+      <div className="weather-display">
+        <div>
+          <h4>{JSON.stringify(responseObj.name)}, GA</h4>
+          <p>Wednesday 1:00 PM</p>
+          <p>Cloudy</p>
+        </div>
+        <div className="weather-data">
+          {/* <img alt="temp-data"/> */}
+          53
+        </div>
+      </div>
+      <div className="weatherJSON">{JSON.stringify(responseObj)}</div>
     </div>
   );
 };
